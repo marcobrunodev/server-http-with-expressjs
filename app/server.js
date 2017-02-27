27892f741aa33d2eb15841c3185753ff12a6ecee
Marco Bruno <marco.bruno.br@gmail.com>
2017-02-27 01:47:02 -0300

    Created response to route /

--- app/server.js
+++ app/server.js
@@ -2,6 +2,10 @@ let express = require('express')
 let app = express()
 let port = 3000
 
+app.get('/', (req, res) => {
+  res.send('<h1>Home</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
   console.log('Para derrubar o servidor: ctrl + c');
