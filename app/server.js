0c06aba72e1fcf13d084264fcedebb709b798d11
Marco Bruno <marco.bruno.br@gmail.com>
2017-02-27 02:10:51 -0300

    [GET] - Created response to route /contato

--- app/server.js
+++ app/server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('<h1>Home</h1>')
 })
 
+app.get('/contato', (req, res) => {
+  res.send(`
+    <h1>Contato</h1>
+
+    <form action="/contato" method="POST">
+      <label for="email">Email:</label>
+      <input type="email" name="email" id="email">
+
+      <label for="mensagem">Mensagem:</label>
+      <textarea name="mensagem" id="mensagem"></textarea>
+
+      <input type="submit" value="Enviar">
+    </form>
+  `)
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
   console.log('Para derrubar o servidor: ctrl + c');
