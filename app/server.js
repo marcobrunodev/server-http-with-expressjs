43e9132f29db8a2b6cb5a1a541f8d850c74923db
Stain António Kozan AK <70895080+lemillioncorp@users.noreply.github.com>
2021-06-14 01:31:41 +0100

    Atualizando de Send para SendFile
    Enviando Arquivos ao invés de Marcas direitas

--- app/server.js
+++ app/server.js
@@ -3,28 +3,12 @@ let app = express()
 let port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home</h1>')
+  res.sendFile(__dirname+'/views/index.html')
 })
 
-app.get('/contato', (req, res) => {
-  res.send(`
-    <h1>Contato</h1>
-
-    <form action="/contato" method="POST">
-      <label for="email">Email:</label>
-      <input type="email" name="email" id="email">
-
-      <label for="mensagem">Mensagem:</label>
-      <textarea name="mensagem" id="mensagem"></textarea>
-
-      <input type="submit" value="Enviar">
-    </form>
-  `)
-})
-
-app.post('/contato', (req, res) => {
-  res.send('<h1>Precisamos aprender a pegar os valor que usuário digitou!</h1>')
-})
+app.get('/sobre', (req, res) => {
+ res.sendFile(__dirname+'/views/sobre.html')
+}
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
